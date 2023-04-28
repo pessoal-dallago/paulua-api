3281c645713fdb3099fe25f2befc48db410bf18b
paulodallago <paulohdallago@gmail.com>
2023-04-28 14:36:08 -0300

    add comments

--- src/main/java/com/paulolua/pauloluaapi/models/Image.java
+++ src/main/java/com/paulolua/pauloluaapi/models/Image.java
@@ -1,20 +1,29 @@
+//define o nome do pacote. coisa do springboot, não precisa entender.
 package com.paulolua.pauloluaapi.models;
 
+//importações. é como o #include do c++, serve pra usar comandos que não existem no Java padrão
+//muito raramente vc precisa escrever isso manualmente. uma IDE bem configurada faz isso sozinha (ou apertando com o botão direito no código que precisa da importação [ou ctrl + .])
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+//específica o nome da coleção do banco de dados, nesse caso, é images.
+//esse @ é chamado uma annotation, é uma funcionalidade do springboot que faz tipo uma mágica, faz as coisas sozinho. define o q tem q ser feito e faz sozinho, por isso não tem ; no fim
 @Document(collection="images")
+//aqui de fato é criada a classe Image
+//public significa que pode ser acessada por todas as outras classes do projeto
 public class Image {
 
+    //aqui cria as variáveis que vão criar um objeto imagem
     @Id
     private String id;
     private String url;
     private String label;
 
     public Image() {
-        
+        // ???
     }
 
+    //aqui é o contructor, que constrói a imagem, recebendo valores como parametro que são atribuídos às variáveis da classe.
     public Image(String id, String url, String label){
 
         this.id=id;
@@ -22,6 +31,7 @@ public class Image {
         this.label=label;
     }
 
+    //esses são os métodos. é bem simples: getId, por exemplo, vc quer receber o id, o método retorna o id. simples.
     public String getId(){
         return id;
     }
